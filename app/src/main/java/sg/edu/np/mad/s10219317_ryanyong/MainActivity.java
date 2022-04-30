7e8f9b4646e8e916791b0758c25e277e893f461a
Ryanyzh <S10219317@connect.np.edu.sg>
2022-04-30 16:12:31 +0800

    Complete Practical 02

---
+++ app/src/main/java/sg/edu/np/mad/s10219317_ryanyong/MainActivity.java
@@ -0,0 +1,34 @@
+package sg.edu.np.mad.s10219317_ryanyong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    User user = new User("Ryan", "Is clueless??", 10, true);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void hiClick(View v){
+        //event handler
+        user.followed = !(user.followed);
+        TextView txt = findViewById(R.id.follow_btn);
+        if (!user.followed){
+            txt.setText("Follow");
+        }
+        else{
+            txt.setText("Unfollow");
+        }
+
+    }
+
+}
